381006cdba9e20822f1f1aeb3361daf0bcd012cc
iresha <ireshagayanisen223@gmail.com>
2021-11-30 20:30:51 -0500

    Initial Commit

---
+++ projIresha/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  projIresha
+//
+//  Created by user194312 on 11/30/21.
+//  Copyright © 2021 user194312. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
